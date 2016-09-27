32ee4b7223c9bb209e42b89c3061796a6777330f
Anton Bezrukov <hello@antonbezrukov.ru>
2016-09-28 01:08:24 +0300

    Add stdin logging feature

--- main.go
+++ main.go
@@ -1,42 +1,95 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"fmt"
+	"io"
+	"os"
+	"sync"
+	"time"
 )
 
 type config struct {
-	path          string
-	isListening   bool
-	listen_host   string
-	listen_port   int
-	isSending     bool
-	send_host     string
-	send_port     int
-	add_timestamp bool
-	node_name     string
-	period        string
+	logfile      string
+	isListening  bool
+	listenHost   string
+	listenPort   int
+	isSending    bool
+	sendHost     string
+	sendPort     int
+	addTimestamp bool
+	nodeName     string
+	period       time.Duration
 }
 
 func main() {
 	config := set_flags()
-	fmt.Println(config.isListening, config.isSending, config.listen_port, config.listen_host, config.send_host, config.send_port, config.add_timestamp, config.node_name)
+
+	logChan := make(chan string, 10000)
+	var wg sync.WaitGroup
+
+	go startLogWriter(logChan, config, &wg)
+	processStdin(logChan, config, &wg)
+
+	wg.Wait()
 }
 
 func set_flags() *config {
 	config := config{}
 
 	flag.BoolVar(&config.isListening, "listen", false, "Description")
-	flag.StringVar(&config.listen_host, "listen-host", "localhost", "Description")
-	flag.IntVar(&config.listen_port, "listen-port", 22016, "Description")
+	flag.StringVar(&config.listenHost, "listen-host", "localhost", "Description")
+	flag.IntVar(&config.listenPort, "listen-port", 22016, "Description")
 	flag.BoolVar(&config.isSending, "send", false, "Description")
-	flag.StringVar(&config.send_host, "send-host", "localhost", "Description")
-	flag.IntVar(&config.send_port, "send-port", 22016, "Description")
-	flag.BoolVar(&config.add_timestamp, "timestamp", true, "Description")
-	flag.StringVar(&config.node_name, "name", "", "Description")
-	flag.StringVar(&config.path, "path", "./simplog.log", "Description")
-	flag.StringVar(&config.period, "period", "none", "Description 1d 1w")
+	flag.StringVar(&config.sendHost, "send-host", "localhost", "Description")
+	flag.IntVar(&config.sendPort, "send-port", 22016, "Description")
+	flag.BoolVar(&config.addTimestamp, "timestamp", true, "Description")
+	flag.StringVar(&config.nodeName, "name", "", "Description")
+	flag.StringVar(&config.logfile, "logfile", "./simplog.log", "Description")
+	flag.DurationVar(&config.period, "period", time.Duration(24*365*42)*time.Hour, "Description 1d 1w")
 
 	flag.Parse()
 	return &config
+}
+
+func startLogWriter(records chan string, config *config, wg *sync.WaitGroup) {
+	logFile, err := os.OpenFile(config.logfile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0664)
+	if err != nil {
+		panic("Can't open the log file \"" + config.logfile + "\". " + err.Error())
+	}
+
+	for {
+		logRecord := <-records
+		_, err := logFile.WriteString(logRecord)
+		if err != nil {
+			panic("Can't write to the log file. " + err.Error())
+		}
+		wg.Done()
+	}
+}
+
+func processStdin(logChan chan string, config *config, wg *sync.WaitGroup) {
+	inReader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := inReader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+
+		wg.Add(1)
+		logChan <- makeLogString(line, config)
+	}
+}
+
+func makeLogString(text string, config *config) string {
+	logString := text
+
+	if config.nodeName != "" {
+		logString = config.nodeName + ": " + logString
+	}
+	if config.addTimestamp {
+		logString = time.Now().UTC().String() + " - " + logString
+	}
+
+	return logString
 }
